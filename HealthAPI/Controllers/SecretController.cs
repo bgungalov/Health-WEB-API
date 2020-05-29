6f7d9270e9aad86ea3b29f635ce641d0440d5d43
gungalovelectronics <gungalovelectronics@gmail.com>
2020-05-29 11:54:43 +0300

    3rd

---
+++ HealthAPI/Controllers/SecretController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using HealthAPI.Filters;
+
+namespace HealthAPI.Controllers
+{
+    [ApiKeyAuth]
+    public class SecretController : ControllerBase
+    {
+        [HttpGet("secret")]
+        public IActionResult GetSecret()
+        {
+            return Ok("I have no secrets");
+        }
+    }
+}
